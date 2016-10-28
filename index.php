8b6987fb6e0de1716a8ecaccf56b0ee954fa9b51
Alexander Marenin <alexsoftinc@users.noreply.github.com>
2016-10-28 19:59:32 +0500

    Create index.php
    This is general load config file )

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'config.php';
+include_once 'test_mobile.php';
+$get_settings = DB::run()->query("SELECT * FROM tbl_settings WHERE id = '1'")->fetch(PDO::FETCH_OBJ);
+
+if($get_settings->site_off == '0') {
+    include_once 'views/error/index.php';
+} else {
+    function __autoload($class) {
+        include_once 'libs/'.$class.'.php';
+    }
+
+    $app = new Bootstrap();
+}
